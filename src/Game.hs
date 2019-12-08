2d682e404b508a45ad1045d23004823cc3919bc4
Dylan Turner <dylantdmt@gmail.com>
2019-12-08 16:17:58 -0500

    Implemented basic scrolling to follow player

--- src/Game.hs
+++ src/Game.hs
@@ -1,7 +1,7 @@
 module Game where
 
 import Graphics.Gloss.Interface.Pure.Game(play, Event)
-import Graphics.Gloss   ( Picture, pictures, circleSolid, translate
+import Graphics.Gloss   ( Picture(..), pictures, circleSolid, translate
                         , red, color )
 import Graphics.Gloss.Data.ViewPort
 
@@ -9,17 +9,26 @@ import State(GameWindow(..), GameState(..), Sprite(..), GameObject(..), drawSpri
 import Globals(startState)
 import Input(handler)
 
+-- Adjust viewport to follow the player
+scrollToPlayer :: Picture -> GameState -> Picture
+scrollToPlayer img state =
+    applyViewPortToPicture newViewPort img
+    where
+        (px, py) = objPosition $ player state
+
+        newViewPort = viewPortInit  { viewPortTranslate =   (-px, py) }
+
 -- Draw everything important on screen
 -- Will eventually "iterate" through a list of GameObjects and draw their sprites
 -- Will be something like "map (drawGameObject state) gameObjectList"
 render :: GameState -> Picture
 render state =
     -- Test basic drawing and changed viewport/coordinate system
-    pictures    --[ color red (circleSolid 100)
-                --, translate 100 (-100) (color red (circleSolid 100)) 
-                --, drawSprite state $ sprCubeManIdle state
-                --, drawSprite state $ sprCubeManWalk state ]
-                [ drawGameObject state $ player state ]
+    scrollToPlayer (pictures    [ Blank -- This is just here so when I'm testing and there's not much stuff, I can keep the formatting the same
+                                        -- Player should be drawn last, so I have to redo the formatting everytime I remove test objects
+                                , drawGameObject state $ player state ]) state
+    where
+        (px, py) = objPosition $ player state
 
 -- This updates all the moving objects in the system
 -- It also increases animCounter which allows render to display the correct frame
@@ -44,15 +53,8 @@ update dt state =
 --  * update function
 startGame :: IO()
 startGame =
-    play disp bg numframes startState renderViewPort handler update
+    play disp bg numframes startState render handler update
     where
         disp =      display $ window startState
         bg =        bgColor $ window startState
         numframes = fps     $ window startState
-        winWidth =  width   $ window startState
-        winHeight = height  $ window startState
-
-        -- Shift everything over to get regular electronic coordinate systems
-        newViewPort = viewPortInit  { viewPortTranslate =   (-winWidth / 2, winHeight / 2) }
-        renderViewPort state =
-            applyViewPortToPicture newViewPort (render state)
